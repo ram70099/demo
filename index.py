2bd1bc25fb154e0835d66abf5975ac7ba827c4b6
Ram <ramrattan099@gmail.com>
2025-12-03 16:46:22 +0530

    Update index.py

--- index.py
+++ index.py
@@ -1,1 +1,1 @@
-print("Hello, Git! Thi is my first file to practice Git and GitHub.I am learning how to commit, push, and handle conflicts.")
+print("Hello, Git! Thi isfirst file to practice Git and GitHub.I am  how to commit, push, and handle conflicts.")
